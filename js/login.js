e4792ece35c3926f107dcc11fc76260c2545fc13
michaelGit2 <bwibomichael7@gmail.com>
2018-10-04 15:59:18 +0300

    links all pages every collaborator worked on

--- js/login.js
+++ js/login.js
@@ -1,25 +1,27 @@
 function toggle(){
-  var hidePassWord = document.getElementById("password")
+  var hidePassWord = document.getElementById("password");
   if(hidePassWord.type === "password"){
-    hidePassWord.type = "text"
+    hidePassWord.type = "text";
   }else{
-    hidePassWord.type = "password"
+    hidePassWord.type = "password";
   }
 }
 
 function getInfo() {
-  var passWord = "1234"
-  var userName = "admin"
-  var showPassword = "hide"
+  var passWord = "1234";
+  var userName = "admin";
+  var showPassword = "hide";
   var inputed_username = document.getElementById("username").value;
   var iputed_PassWord = document.getElementById("password").value;
   if((userName == inputed_username) && (passWord == iputed_PassWord)){
 
-  result.innerHTML="login successfull"
+  // result.innerHTML="login successfull"
+  alert("login successfull");
+  window.open("table.html");
   }
 else{
 
-  result.innerHTML = "username or password incorrect"
+  result.innerHTML = "username or password incorrect";
 }
 
 }
